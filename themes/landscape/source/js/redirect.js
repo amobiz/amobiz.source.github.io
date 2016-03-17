76f9cdd079fa8b5a7f58d81f55b92bbea95d5c41
amobiz <amobiz.tw+github@gmail.com>
2016-03-17 14:25:32 +0800

    save redirect.js

--- themes/landscape/source/js/redirect.js
+++ themes/landscape/source/js/redirect.js
@@ -1,21 +1,28 @@
+/**
+ * This script is for old blog sites and not used here. Keep here for reference.
+ */
 (function(url, timeout, container) {
-    var div = document.createElement('div');
-    div.innerHTML = '<p>redirect to <span id="location"></span> in <span id="timer"></span> seconds.</p>';
-    container.insertBefore(div, container.firstChild);
+	if (window.location.href.indexOf(url) !== -1) {
+		return;
+	}
 
-    var timer = div.querySelector('#timer');
-    var location = div.querySelector('#location');
+	var div = document.createElement('div');
+	div.innerHTML = '<p>redirect to <span id="location"></span> in <span id="timer"></span> seconds.</p>';
+	container.insertBefore(div, container.firstChild);
 
-    location.innerText = url;
+	var timer = div.querySelector('#timer');
+	var location = div.querySelector('#location');
 
-    updateTimer();
-    setInterval(updateTimer, 1000);
+	location.innerText = url;
 
-    setTimeout(function() {
-        window.location = url;
-    }, 1000 * timeout);
+	updateTimer();
+	setInterval(updateTimer, 1000);
 
-    function updateTimer() {
-        timer.innerText = timeout--;
-    }
+	setTimeout(function() {
+		window.location = url;
+	}, 1000 * timeout);
+
+	function updateTimer() {
+		timer.innerText = timeout--;
+	}
 })('https://amobiz.github.io', 15, document.body);
